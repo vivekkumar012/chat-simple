a00533cdeacf4ff427422644c55cb7af162dad77
Vivek Kumar <vh118854@gmail.com>
2025-09-11 02:13:30 +0530

    changes in index page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hi")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080})
+
+wss.on("connection", (socket) => {
+    console.log("User Connected");
+
+    socket.on("message", (message) => {
+        console.log("message received" +  message.toString());
+    })
+})
