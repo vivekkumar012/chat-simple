e1453808a0bf96cd5e8fdf740b51bf246d0b9ef8
Vivek Kumar <vh118854@gmail.com>
2025-09-13 00:21:22 +0530

    index page updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080})
 
+let allSockets: WebSocket[] = [];
+
 wss.on("connection", (socket) => {
-    console.log("User Connected");
+    console.log("Server Connected");
+    allSockets.push(socket);
 
     socket.on("message", (message) => {
-        console.log("message received" +  message.toString());
+        for(let i=0; i<allSockets.length; i++) {
+            const s = allSockets[i];
+
+            s?.send(message.toString()+":sent from the server");
+        }
     })
 })
