23b724a420e0817754a0e75224096eb00c4f7d7c
Vivek Kumar <vh118854@gmail.com>
2025-09-14 20:18:30 +0530

    new codes are added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,47 @@
 
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [messages, setMessages]  = useState(["hi there"]);
+  const wsRef = useRef();
  
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data]);
+    }
+    wsRef.current = ws;
+
+    ws.onmessage = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
 
   }, []);
 
   return (
     <div className='h-screen bg-black'>
-      <div className='h-[90vh]'></div>
+      <br /> <br />
+      <div className='h-[90vh]'>
+        {messages.map(message => <div className='m-8'> <span className='bg-white text-black p-8 rounded'>{message}</span> </div>)}
+      </div>
       <div className='flex gap-4 p-4'>
-        <input type="text" className='w-full bg-white rounded-2xl' />
-        <button className='bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-700'>Send Message</button>
+        <input id='message' type="text" className='w-full bg-white rounded-2xl' />
+        <button onClick={() => {
+          const message = document.getElementById("message")?.value;
+          wsRef.current.send(JSON.stringify({
+            type: "chat",
+            payload: {
+              message: message
+            }
+          }))
+        }} className='bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-700'>Send Message</button>
       </div>
     </div>
   )
